f552c1cdc5584a8e1dcb316025e2cc1f4393af2a
smesoftwarecharlie <charliemckennakvoriak@gmail.com>
2018-01-25 17:52:58 -0800

    Theo file

---
+++ weapon_theo.py
@@ -0,0 +1,105 @@
+class weapon():
+
+class sword(weapon):
+	def __init__(self):
+		self.damage = 5
+		self.durability = 50
+		self.speed = 6
+		self.dtype = "TBD"
+
+class dagger(sword):
+	def __init__(self):
+		self.damage = 5
+		self.durability = 50
+		self.speed = 6
+		self.dtype = "stab"
+
+class medieval_sword(sword)
+	def __init__(self):
+		self.damage = 5
+		self.durability = 50
+		self.speed = 6
+		self.dtype = "slice"
+
+class rusty_medieval_sword(medieval_sword)
+
+
+
+class club(weapon):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class blaster(weapon):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
