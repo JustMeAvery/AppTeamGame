4138e46ee289182e9445f2836573ecaba654c552
Theodore1237 <35584780+Theodore1237@users.noreply.github.com>
2018-03-27 18:01:43 -0700

    Update theo_enemy.py

--- theo_enemy.py
+++ theo_enemy.py
@@ -2,4 +2,17 @@ class enemy:
 
 class dragon(enemy):
     def __init__(self):
-        self.hp = 210
+        self.hp = 500
+        self.weapon = "dagger"
+        self.abilities = "can breathe fire and dodge attacks by flying out of the way."
+        
+class skeleton(enemy):
+    def __init__(self):
+        self.hp = 450
+        self.weapon = "bow_and_arrow"
+        self.abilities = "can come apart and come together again in a different spot."
+        
+class zombie(enemy):
+    def __init__(self):
+        self.hp = 1000
+        self.weapon = "spikey_club"
