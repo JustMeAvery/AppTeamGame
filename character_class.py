905361af979c4ab32239db563b8e4f5bfc56865b
Theodore1237 <35584780+Theodore1237@users.noreply.github.com>
2018-03-27 18:49:39 -0700

    Update character_class.py

--- character_class.py
+++ character_class.py
@@ -1,2 +1,16 @@
+class character():
+	def __init__(self, name, species, armor):
+		self.hp = 500
+		self.items = ""
+		self.name = name
+		self.weapon = "hi"
+		self.species = species
+		self.armor = armor
 
+
+theo = character("theo", "human", "breastplate, helm, and helmet")
+print (theo.name)
+print (theo.weapon)
+print (theo.species)
+print (theo.armor)
       
